12ff8c223d0d0559b42017c6ba39b23eb8065dd3
Matt <mattmohoney21@gmail.com>
2017-06-26 11:21:08 -0500

    - Setup solution assemblies

---
+++ src/WebLibrary/Areas/CheckLists/CheckListsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebLibrary.Areas.CheckLists
+{
+    public class CheckListsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CheckLists";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CheckLists_default",
+                "CheckLists/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/WebLibrary/Areas/CheckLists/Controllers/CheckListController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebLibrary.Areas.CheckLists.Controllers
+{
+    public class CheckListController : ApiController
+    {
+        // GET: api/CheckList
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/CheckList/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/CheckList
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/CheckList/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/CheckList/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
