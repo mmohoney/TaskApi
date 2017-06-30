87fed31f53de5d5e1abf8ea30d0a0387b59e5fbf
Matt Mohoney <mattmohoney21@gmail.com>
2017-06-29 20:49:19 -0500

    - TeamCity fix

---
+++ src/Service.Test/EmptyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Service.Test
+{
+    [TestFixture]
+    public class EmptyTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
